23aaa3f627ef7cc0a1b4dd44789f9fa33ee4211e
Monique <monique.meas@gmail.com>
2022-09-03 21:45:21 -0400

    add Tag api routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
@@ -18,7 +17,8 @@ ProductTag.init(
       references: {
         model: 'product',
         key:'id'
-      }
+      },
+      
     },
     tag_id: {
       type: DataTypes.INTEGER,
@@ -26,6 +26,7 @@ ProductTag.init(
         model: 'tag',
         key: 'id'
       }
+      
     }
   },
   {
--- models/index.js
+++ models/index.js
@@ -4,29 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Categories have many Products
 
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'  
+});
+
+// Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-  
-});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   as: 'tags',
-  foreignKey: 'product_id'
+  foreignKey: 'product_id',
+ 
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   as: 'tags',
-  foreignKey: 'tag_id'
+  foreignKey: 'tag_id',
+  
 })
 
 module.exports = {
